8f1c67570483d513f5635154defc1045c67b23e8
Jennifer <jsmit032@ucr.edu>
2016-01-03 08:28:52 -0800

    added build folder with html, js, and css files for overlay animation

---
+++ build/js/app.js
@@ -0,0 +1,50 @@
+function openOverlay(olEl) {
+    $oLay = $(olEl);
+    
+    if ($('#overlay-shade').length == 0)
+        $('body').prepend('<div id="overlay-shade"></div>');
+
+    $('#overlay-shade').fadeTo(300, 0.0, function() {
+        var props = {
+            oLayWidth       : $oLay.width(),
+            scrTop          : $(window).scrollTop(),
+            viewPortWidth   : $(window).width()
+        };
+
+        var leftPos = (props.viewPortWidth - props.oLayWidth) / 2;
+
+        $oLay
+            .css({
+                display : 'block',
+                opacity : 0,
+                right : '-=300',
+                //left : leftPos+'px'
+            })
+            .animate({
+                right : props.scrTop + 40,
+                opacity : 0.6
+            }, 600);
+    });
+}
+
+function closeOverlay() {
+    $('.overlay').animate({
+        right : '-=300',
+        opacity : 0.6
+    }, 400, function() {
+        $('#overlay-shade').fadeOut(300);
+        $(this).css('display','none');
+    });
+}
+
+$('#overlay-shade, .overlay a').live('click', function(e) {
+    closeOverlay();
+    if ($(this).attr('href') == '#') e.preventDefault();
+});
+
+
+// Usage
+$('#overlaylaunch-inAbox').click(function(e) {
+   openOverlay('#overlay-inAbox');
+   e.preventDefault();
+});
